769e0a177b2a29130ed83148ae6201b2d31924ff
dagibbs22 <dagibbs22@gmail.com>
2018-10-17 11:29:30 -0400

    Script is working for loss years 2007-2017, using the most recent PRODES raster. Haven't finished writing the part that calculated emissions based on the 2001-2014 PRODES raster.

--- Brazil_GFW_emissions_comparison.py
+++ Brazil_GFW_emissions_comparison.py
@@ -1,124 +1,143 @@
+
+
+
 import os
 import datetime
 import arcpy
-from arcpy import env
+import utilities
 from arcpy.sa import *
 
-arcpy.CheckOutExtension("Spatial")
+if os.path.exists("{0}.gdb".format(utilities.gdb_path)):
+    print "Geodatabase already exists"
+else:
+    # Creates raster mosaics of Hansen loss and burn year out of the necessary tiles
+    print "Creating geodatabase and loss and fire raster mosaics"
+    utilities.create_mosaics()
 
-dir = "C:\\GIS\\GFW_Climate\\Brazil_emis_comparison"
+# # Creates PRODES primary forest layers for 2007 to 2017 using the most recently released PRODES raster.
+# # This only needs to be run once to create the annual PRODES primary forest rasters.
+# utilities.create_annual_PRODES_recent()
 
-gdb = "loss_mosaics"
-Hansen_mosaic = "Hansen_loss_2017"
-burn_mosaic = "burn_loss_from_s3"
-gdb_path = os.path.join(dir, gdb)
-Hansen_loss_tile_path = os.path.join(dir, Hansen_mosaic)
-fire_loss_tile_path = os.path.join(dir, burn_mosaic)
-loss_tile = "C:\\GIS\\GFW_Climate\\Brazil_emis_comparison\\Hansen_loss_2017\\00N_050W.tif"
+# Iterates through years to get loss under various exclusion criteria in each year
+for year in range(2007, 2018):
 
-legal_Amazon_reproj = "C:\\GIS\\Multi_project\\Brazil_legal_Amazon\\prodes_full_extent_reproj.shp"
+    print "Processing loss in", year
 
-prim_2007_17 = os.path.join(dir, "PDigital2017_AMZ_30m.tif")
-prim_2007_17_reclass = os.path.join(dir, "PDigital2017_AMZ_30m_reclass.tif")
-
-loss_noFire = os.path.join(dir, "loss_noFire.tif")
-loss_noFire_legal = os.path.join(dir, "loss_noFire_legalAMZ.tif")
-
-non_burn_area = os.path.join(dir, "non_burn_area_01_17.tif")
-
-# print "Creating geodatabase for loss and fire mosaics in Amazonia..."
-# out_coor_system = arcpy.Describe(loss_tile).spatialReference
-# print "  Creating gdb..."
-# arcpy.CreateFileGDB_management(dir, gdb)
-#
-# print "  Creating Hansen loss mosaic..."
-# arcpy.CreateMosaicDataset_management("{}.gdb".format(gdb_path), Hansen_mosaic, out_coor_system, num_bands="1",
-#                                      pixel_type="8_BIT_UNSIGNED")
-# print "  Adding Hansen loss tiles to mosaic..."
-# arcpy.AddRastersToMosaicDataset_management("{0}.gdb/{1}".format(gdb_path, Hansen_mosaic),
-#                                            raster_type="Raster Dataset", input_path=Hansen_loss_tile_path)
-#
-# print "  Creating fire loss mosaic..."
-# arcpy.CreateMosaicDataset_management("{}.gdb".format(gdb_path), burn_mosaic, out_coor_system, num_bands="1",
-#                                      pixel_type="8_BIT_UNSIGNED")
-# print "  Adding fire loss tiles to mosaic..."
-# arcpy.AddRastersToMosaicDataset_management("{0}.gdb/{1}".format(gdb_path, burn_mosaic),
-#                                            raster_type="Raster Dataset", input_path=fire_loss_tile_path)
-#
-# print "Clipping loss raster to loss outside of fires"
-# print "Start time is", str(datetime.datetime.now())
-# print "  Creating raster of non-fire areas"
-# outCon1 = arcpy.gp.Con_sa("{0}.gdb/{1}".format(gdb_path, burn_mosaic), "999", non_burn_area, "", "VALUE = 0")
-# print "  Saving raster of non-fire areas"
-# outCon1.save(non_burn_area)
-# print "  Creating raster of loss in non-fire areas"
-# arcpy.gp.ExtractByMask_sa("{0}.gdb/{1}".format(gdb_path, Hansen_mosaic), non_burn_area, loss_noFire)
-# print "End time is", str(datetime.datetime.now())
-
-# print "Clipping non-fire loss raster to legal Amazon"
-# print "Start time is", str(datetime.datetime.now())
-# arcpy.gp.ExtractByMask_sa(loss_noFire, legal_Amazon_reproj, loss_noFire_legal)
-# print "End time is", str(datetime.datetime.now())
-#
-# This definition of PRODES primary forest is for the end of 2006. Primary forest is anything lost in 2007 or later or still primary forest
-# print "Reclassifying PRODES primary forest raster"
-# outReclass = Reclassify(prim_2007_17, "Value", RemapRange([[1,1,1], [2,5,"NODATA"], [6,24,1]]))
-# print "Saving reclassified primary forest raster"
-# outReclass.save(prim_2007_17_reclass)
-
-for year in range(2015, 2018):
-
-    loss_noFire_legal_year = os.path.join(dir, "loss_noFire_legalAMZ_{}.tif".format(year))
-    loss_noFire_legal_year_PRODES = os.path.join(dir, "loss_noFire_legalAMZ_{}_PRODES.tif".format(year))
-    loss_noFire_legal_year_PRODES_neighbor = os.path.join(dir, "loss_noFire_legalAMZ_{}_PRODES_neighbor.tif".format(year))
-    loss_noFire_legal_year_PRODES_neighbor_shp = os.path.join(dir, "loss_noFire_legalAMZ_{}_PRODES_neighbor_convert.shp".format(year))
-    loss_noFire_legal_year_PRODES_neighbor_shp_dissolve = os.path.join(dir, "loss_noFire_legalAMZ_{}_PRODES_neighbor_convert_diss.shp".format(year))
-    loss_noFire_legal_year_PRODES_neighbor_shp_dissolve_reproj = os.path.join(dir, "loss_noFire_legalAMZ_{}_PRODES_neighbor_convert_diss_reproj.shp".format(year))
-    loss_noFire_legal_year_PRODES_neighbor_shp_dissolve_reproj_areas = os.path.join(dir, "loss_noFire_legalAMZ_{}_PRODES_neighbor_convert_diss_reproj_areas.shp".format(year))
-    loss_noFire_legal_year_PRODES_neighbor_shp_dissolve_reproj_areas_large = os.path.join(dir, "loss_noFire_legalAMZ_{}_PRODES_neighbor_convert_diss_reproj_areas_large.shp".format(year))
-
+    # One or two digit year used for processing
     short_year = year - 2000
 
-    print "Masking loss raster to loss in year {}".format(year)
-    print "Start time is", str(datetime.datetime.now())
-    arcpy.gp.Con_sa(loss_noFire_legal, "1", loss_noFire_legal_year, "", '"Value"={}'.format(short_year))
-    print "End time is", str(datetime.datetime.now())
+    # Names for output files under each criteria. Used for both tif and shp.
+    loss_year = os.path.join(utilities.out_dir, "loss_{}".format(year))
+    loss_year_noFire = os.path.join(utilities.out_dir, "loss_{}_noFire".format(year))
+    loss_year_noFire_legal = os.path.join(utilities.out_dir, "loss_{}_noFire_legalAMZ".format(year))
+    loss_year_noFire_legal_PRODES = os.path.join(utilities.out_dir, "loss_{}_noFire_legalAMZ_PRODES".format(year))
+    loss_year_noFire_legal_PRODES_neighbor = os.path.join(utilities.out_dir, "loss_{}_noFire_legalAMZ_PRODES_neighbor".format(year))
+    loss_year_noFire_legal_PRODES_neighbor_shp = os.path.join(utilities.out_dir, "loss_{}_noFire_legalAMZ_PRODES_neighbor_convert.shp".format(year))
+    loss_year_noFire_legal_PRODES_neighbor_shp_dissolve = os.path.join(utilities.out_dir, "loss_{}_noFire_legalAMZ_PRODES_neighbor_convert_diss.shp".format(year))
+    loss_year_noFire_legal_PRODES_neighbor_shp_dissolve_reproj = os.path.join(utilities.out_dir, "loss_{}_noFire_legalAMZ_PRODES_neighbor_convert_diss_reproj.shp".format(year))
+    loss_year_noFire_legal_PRODES_neighbor_shp_dissolve_reproj_areas = os.path.join(utilities.out_dir, "loss_{}_noFire_legalAMZ_PRODES_neighbor_convert_diss_reproj_areas.shp".format(year))
+    loss_year_noFire_legal_PRODES_neighbor_shp_dissolve_reproj_areas_large = os.path.join(utilities.out_dir, "loss_{}_noFire_legalAMZ_PRODES_neighbor_convert_diss_reproj_areas_large.shp".format(year))
 
-    print "Clipping loss raster to loss in PRODES primary forest"
-    print "Start time is", str(datetime.datetime.now())
-    outExtractByMask3 = ExtractByMask(loss_noFire_legal_year, prim_2007_17_reclass)
-    print "Saving loss raster clipped to PRODES primary forest"
-    outExtractByMask3.save(loss_noFire_legal_year_PRODES)
-    print "End time is", str(datetime.datetime.now())
+    # Annual PRODES primary forest extents
+    PRODES_prim = os.path.join(utilities.dir, utilities.PRODES_folder, "PRODES_primary_forest_{}.tif".format(year))
 
-    print "Grouping neighboring {} loss cells".format(year)
-    print "Start time is", str(datetime.datetime.now())
-    outRegionGrp = RegionGroup(loss_noFire_legal_year_PRODES, "EIGHT", "WITHIN", "NO_LINK")
+    # Masks loss to the given year
+    print "  Masking loss to", year
+    print "    Start time is", str(datetime.datetime.now())
+    arcpy.gp.Con_sa("{0}.gdb/{1}".format(utilities.gdb_path, utilities.Hansen_mosaic), "{0}.gdb/{1}".format(utilities.gdb_path, utilities.Hansen_mosaic),
+                    "{}.tif".format(loss_year), "",
+                    '"Value" = {}'.format(short_year))
+    print "    End time is", str(datetime.datetime.now())
+
+    # Masks loss to pixels that were not burned (Brazil exclusion criteria 1).
+    # Uses a merged burn year raster instead of a raster mosaic dataset because I couldn't get Con to work on the mosaic for some reason.
+    # However, I did get it to work on a single raster of all the burn tiled merged after I create an attribute table for it,
+    # so I used the merged raster instead.
+    # For loss in 2001, only considers burn pixels from 2001 because considering previous year burning would include "0",
+    # which means no burning was observed.
+    if short_year == 1:
+
+        print "  Removing loss pixels with fire in", year, "or", year-1
+        print "    Start time is", str(datetime.datetime.now())
+        arcpy.gp.Con_sa(os.path.join(utilities.dir, utilities.burnyear_merge), "{}.tif".format(loss_year),
+                        "{}.tif".format(loss_year_noFire), "",
+                        '"Value" NOT IN ({0})'.format(short_year))
+        print "    End time is", str(datetime.datetime.now())
+
+    # For loss in all later years (after 2001), considers burn pixels from the loss year and the preceding year.
+    # Uses burn pixels from the loss year and previous year because loss could have occurred in the previous year but not been observed until the following year.
+    else:
+
+        print "  Removing loss pixels with fire in", year, "or", year-1
+        print "    Start time is", str(datetime.datetime.now())
+        arcpy.gp.Con_sa(os.path.join(utilities.dir, utilities.burnyear_merge), "{}.tif".format(loss_year),
+                        "{}.tif".format(loss_year_noFire), "",
+                        '"Value" NOT IN ({0}-1, {0})'.format(short_year))
+        print "    End time is", str(datetime.datetime.now())
+
+    # Clips loss to legal Amazon boundary (Brazil exclusion criteria 2)
+    print "  Clipping loss raster to legal Amazon"
+    print "    Start time is", str(datetime.datetime.now())
+    arcpy.gp.ExtractByMask_sa("{}.tif".format(loss_year_noFire), utilities.legal_Amazon_reproj, "{}.tif".format(loss_year_noFire_legal))
+    print "    End time is", str(datetime.datetime.now())
+
+    # Clips loss to where there is PRODES primary forest from that year (Brazil exclusion criteria 3)
+    print "  Clipping loss raster to loss in PRODES primary forest"
+    print "    Start time is", str(datetime.datetime.now())
+    arcpy.gp.ExtractByMask("{}.tif".format(loss_year_noFire_legal), PRODES_prim, "{}.tif".format(loss_year_noFire_legal_PRODES))
+    print "    End time is", str(datetime.datetime.now())
+
+    # Groups loss pixels into clusters of contiguous pixels (including diagonal pixels).
+    # Each cluster gets its own ID number.
+    print "  Grouping neighboring loss cells"
+    print "    Start time is", str(datetime.datetime.now())
+    outRegionGrp = RegionGroup("{}.tif".format(loss_year_noFire_legal_PRODES), "EIGHT", "WITHIN", "NO_LINK")
     print "Saving neighboring cells raster"
-    outRegionGrp.save(loss_noFire_legal_year_PRODES_neighbor)
-    print "End time is", str(datetime.datetime.now())
+    outRegionGrp.save("{}.tif".format(loss_year_noFire_legal_PRODES_neighbor))
+    print "    End time is", str(datetime.datetime.now())
 
-    print "Converting {0} loss raster to {0} loss polygon".format(year)
-    print "Start time is", str(datetime.datetime.now())
-    arcpy.RasterToPolygon_conversion(loss_noFire_legal_year_PRODES_neighbor, loss_noFire_legal_year_PRODES_neighbor_shp, raster_field="Value", simplify="NO_SIMPLIFY")
-    print "Dissolving features into contiguous forest features"
-    arcpy.Dissolve_management(loss_noFire_legal_year_PRODES_neighbor_shp, loss_noFire_legal_year_PRODES_neighbor_shp_dissolve, dissolve_field=["gridcode"], multi_part="MULTI_PART")
-    print "End time is", str(datetime.datetime.now())
+    # Converts the grouped annual loss raster into a shapefile
+    print "  Converting {0} loss raster to {0} loss polygon".format(year)
+    print "    Start time is", str(datetime.datetime.now())
+    arcpy.RasterToPolygon_conversion("{}.tif".format(loss_year_noFire_legal_PRODES_neighbor), loss_year_noFire_legal_PRODES_neighbor_shp, raster_field="Value", simplify="NO_SIMPLIFY")
+    print "    Dissolving features into contiguous forest features"
+    arcpy.Dissolve_management(loss_year_noFire_legal_PRODES_neighbor_shp, loss_year_noFire_legal_PRODES_neighbor_shp_dissolve, dissolve_field=["gridcode"], multi_part="MULTI_PART")
+    print "    End time is", str(datetime.datetime.now())
 
-    print "Reprojecting {} loss area to World Eckert IV".format(year)
-    print "Start time is", str(datetime.datetime.now())
+    print "  Reprojecting {} loss area to World Eckert IV".format(year)
+    print "    Start time is", str(datetime.datetime.now())
     out_coordinate_system = "PROJCS['World_Eckert_IV',GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]],PROJECTION['Eckert_IV'],PARAMETER['False_Easting',0.0],PARAMETER['False_Northing',0.0],PARAMETER['Central_Meridian',0.0],UNIT['Meter',1.0]]"
-    arcpy.Project_management(loss_noFire_legal_year_PRODES_neighbor_shp_dissolve, loss_noFire_legal_year_PRODES_neighbor_shp_dissolve_reproj, out_coor_system=out_coordinate_system, transform_method="")
-    print "End time is", str(datetime.datetime.now())
+    arcpy.Project_management(loss_year_noFire_legal_PRODES_neighbor_shp_dissolve, loss_year_noFire_legal_PRODES_neighbor_shp_dissolve_reproj, out_coor_system=out_coordinate_system, transform_method="")
+    print "    End time is", str(datetime.datetime.now())
 
-    print "Calculating feature areas"
-    print "Start time is", str(datetime.datetime.now())
-    arcpy.CalculateAreas_stats(loss_noFire_legal_year_PRODES_neighbor_shp_dissolve_reproj, loss_noFire_legal_year_PRODES_neighbor_shp_dissolve_reproj_areas)
-    print "End time is", str(datetime.datetime.now())
+    print "  Calculating feature areas"
+    print "    Start time is", str(datetime.datetime.now())
+    arcpy.CalculateAreas_stats(loss_year_noFire_legal_PRODES_neighbor_shp_dissolve_reproj, loss_year_noFire_legal_PRODES_neighbor_shp_dissolve_reproj_areas)
+    print "    End time is", str(datetime.datetime.now())
 
-    print "Selecting features larger than 6.25 ha"
-    print "Start time is", str(datetime.datetime.now())
+    # Keeps only the features that are larger than 6.25 ha (Brazil exclusion criteria 4)
+    print "  Selecting features larger than 6.25 ha"
+    print "    Start time is", str(datetime.datetime.now())
     where = "F_AREA > 62500"
-    arcpy.MakeFeatureLayer_management(loss_noFire_legal_year_PRODES_neighbor_shp_dissolve_reproj_areas, "layer_{}".format(year), where_clause=where)
-    arcpy.CopyFeatures_management("layer_{}".format(year), loss_noFire_legal_year_PRODES_neighbor_shp_dissolve_reproj_areas_large)
-    print "End time is", str(datetime.datetime.now())
+    arcpy.MakeFeatureLayer_management(loss_year_noFire_legal_PRODES_neighbor_shp_dissolve_reproj_areas, "layer_{}".format(year), where_clause=where)
+    arcpy.CopyFeatures_management("layer_{}".format(year), loss_year_noFire_legal_PRODES_neighbor_shp_dissolve_reproj_areas_large)
+    print "    End time is", str(datetime.datetime.now())
+
+
+    # Converts all the rasters to shapefiles so that their areas and emissions calculated
+    print "  Converting rasters to shapefiles"
+    arcpy.RasterToPolygon_conversion("{}.tif".format(loss_year),
+                                     "{}.shp".format(loss_year),
+                                     "NO_SIMPLIFY", "VALUE")
+    arcpy.RasterToPolygon_conversion("{}.tif".format(loss_year_noFire),
+                                     "{}.shp".format(loss_year_noFire),
+                                     "NO_SIMPLIFY", "VALUE")
+    arcpy.RasterToPolygon_conversion("{}.tif".format(loss_year_noFire_legal),
+                                     "{}.shp".format(loss_year_noFire_legal),
+                                     "NO_SIMPLIFY", "VALUE")
+    arcpy.RasterToPolygon_conversion("{}.tif".format(loss_year_noFire_legal_PRODES),
+                                     "{}.shp".format(loss_year_noFire_legal_PRODES),
+                                     "NO_SIMPLIFY", "VALUE")
+    arcpy.RasterToPolygon_conversion("{}.tif".format(loss_year_noFire_legal_PRODES_neighbor),
+                                     "{}.shp".format(loss_year_noFire_legal_PRODES_neighbor),
+                                     "NO_SIMPLIFY", "VALUE")
---
+++ utilities.py
@@ -0,0 +1,125 @@
+import os
+import arcpy
+from arcpy.sa import *
+import datetime
+
+path = 'C:\GIS\GFW_Climate\Brazil_emis_comparison'
+
+os.chdir(path)
+
+arcpy.CheckOutExtension("Spatial")
+
+dir = "C:\\GIS\\GFW_Climate\\Brazil_emis_comparison"
+
+out_dir = os.path.join(dir, "output")
+
+gdb = "loss_mosaics"
+Hansen_mosaic = "Hansen_loss_2017"
+burn_mosaic = "burn_loss_from_s3"
+gdb_path = os.path.join(dir, gdb)
+Hansen_loss_tile_path = os.path.join(dir, Hansen_mosaic)
+fire_loss_tile_path = os.path.join(dir, burn_mosaic)
+loss_tile = "C:\\GIS\\GFW_Climate\\Brazil_emis_comparison\\Hansen_loss_2017\\00N_050W.tif"
+PRODES_folder = "PRODES"
+
+# The name of the burn year tiles merged into a single raster.
+# I needed to create the attribute table for this to work in the Con tool.
+burnyear_merge = "burnyear_merge_20181013.tif"
+
+# Legal Amazon boundary
+legal_Amazon_reproj = "C:\\GIS\\Multi_project\\Brazil_legal_Amazon\\prodes_full_extent_reproj_align.tif"
+
+# PRODES primary forest 2000-2013, reprojected to Hansen projection and snapped to Hansen raster
+prim_2000_13 = os.path.join(dir, PRODES_folder, "Prodes2014_AMZ_60m_warp.tif")
+
+# PRODES primary forest 2007-2017, reprojected to Hansen projection and snapped to Hansen raster
+prim_2007_17 = os.path.join(dir, PRODES_folder, "PDigital2017_AMZ_30m_warp.tif")
+
+
+def create_mosaics():
+
+    print "Creating geodatabase for loss and fire mosaics in Amazonia..."
+    out_coor_system = arcpy.Describe(loss_tile).spatialReference
+    print "  Creating gdb..."
+    arcpy.CreateFileGDB_management(dir, gdb)
+
+    print "  Creating Hansen loss mosaic..."
+    arcpy.CreateMosaicDataset_management("{}.gdb".format(gdb_path), Hansen_mosaic, out_coor_system, num_bands="1",
+                                         pixel_type="8_BIT_UNSIGNED")
+    print "  Adding Hansen loss tiles to mosaic..."
+    arcpy.AddRastersToMosaicDataset_management("{0}.gdb/{1}".format(gdb_path, Hansen_mosaic),
+                                               raster_type="Raster Dataset", input_path=Hansen_loss_tile_path)
+
+    # # I couldn't get the burn year mosaic to work with the Con tool (kept making error 001464 or something), so
+    # # this program never actually used the burn year mosaic. Hence, left this in but commented it out.
+    # print "  Creating fire loss mosaic..."
+    # arcpy.CreateMosaicDataset_management("{}.gdb".format(gdb_path), burn_mosaic, out_coor_system, num_bands="1",
+    #                                      pixel_type="8_BIT_UNSIGNED")
+    # print "  Adding fire loss tiles to mosaic..."
+    # arcpy.AddRastersToMosaicDataset_management("{0}.gdb/{1}".format(gdb_path, burn_mosaic),
+    #                                            raster_type="Raster Dataset", input_path=fire_loss_tile_path)
+
+
+# Iterates through 2001-2013 to create PRODES primary forest rasters
+def create_annual_PRODES_early():
+
+    # The years included in the most recent PRODES update
+    for year in range(2001, 2014):
+
+        print "Masking PRODES primary forest raster to what was there just before", year
+
+        # Annual PRODES primary forest extents
+        prim_reclass = os.path.join(dir, PRODES_folder, "PRODES_primary_forest_{}.tif".format(year))
+
+        short_year = year - 2000
+
+        print "  PRODES from 2010 or later"
+        print "  Start time is", str(datetime.datetime.now())
+        outReclass = Reclassify(prim_2007_17, "Value",
+                                RemapRange([[1, 1, short_year],
+                                            [2, short_year - 2, "NODATA"], [short_year - 1, 16, short_year],
+                                            [17, short_year + 6, "NODATA"], [short_year + 7, 24, short_year]]))
+        print "  Saving reclassified primary forest raster"
+        outReclass.save(prim_reclass)
+        print "  End time is", str(datetime.datetime.now())
+
+# Iterates through 2007-2017 to create PRODES primary forest rasters
+def create_annual_PRODES_recent():
+
+    # The years included in the most recent PRODES update
+    for year in range(2007, 2018):
+
+        print "Masking PRODES primary forest raster to what was there just before", year
+
+        # Annual PRODES primary forest extents
+        prim_reclass = os.path.join(dir, PRODES_folder, "PRODES_primary_forest_{}.tif".format(year))
+
+        short_year = year - 2000
+
+        # For 2007-2009, there is no residual deforestation class (r), so the reclassification rules are simpler.
+        # Deforestation is the classes before the focal year (e.g., deforestation to make PRODES 2009 layer is
+        # deforestation in 2007 and 2008 but not 2009).
+        if short_year < 10:
+
+            print "  PRODES before 2010"
+            print "  Start time is", str(datetime.datetime.now())
+            outReclass = Reclassify(prim_2007_17, "Value",
+                                    RemapRange([[1, 1, short_year],
+                                                [2, short_year - 2, "NODATA"], [short_year - 1, 24, short_year]]))
+            print "  Saving reclassified primary forest raster"
+            outReclass.save(prim_reclass)
+            print "  End time is", str(datetime.datetime.now())
+
+        # For 2010 and later, there are annual residual deforestation classes (rxxxx), so the reclassification needs to
+        # account for those
+        if short_year >= 10:
+
+            print "  PRODES from 2010 or later"
+            print "  Start time is", str(datetime.datetime.now())
+            outReclass = Reclassify(prim_2007_17, "Value",
+                                    RemapRange([[1, 1, short_year],
+                                                [2, short_year - 2, "NODATA"], [short_year - 1, 16, short_year],
+                                                [17, short_year + 6, "NODATA"], [short_year + 7, 24, short_year]]))
+            print "  Saving reclassified primary forest raster"
+            outReclass.save(prim_reclass)
+            print "  End time is", str(datetime.datetime.now())
